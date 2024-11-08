4e65b4e1ff1dba5d178e526624db3c71d69eeb45
w0obi <123972232+w0obi@users.noreply.github.com>
2024-11-08 13:14:59 +0900

    Update api-config.js

--- src/component/api-config.js
+++ src/component/api-config.js
@@ -4,7 +4,7 @@ const hostname = window && window.location && window.location.hostname;
 
 // 브라우저의 도메인이 'localhost'일 때 백엔드 서버 주소를 로컬로 설정
 if (hostname === 'localhost') {
-backendHost = 'http://43.200.245.197:8080';
+backendHost = 'http://43.203.226.201:8080';
 }
 
 // 백엔드 API 주소 내보내기
